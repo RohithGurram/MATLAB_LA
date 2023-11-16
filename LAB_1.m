2208f1716abdd5c3821dff32eb3686c4ff50c8d5
RohithGurram <89861100+RohithGurram@users.noreply.github.com>
2023-11-16 12:16:29 +0530

    Update LAB_1.m

--- LAB_1.m
+++ LAB_1.m
@@ -1,35 +1,67 @@
 exp-01
-Tb=1;
-N=10;
-fc=5;
-time=0:0.001:N*Tb;
-Tc=0:0.001:Tb;
-c=sqrt(2/Tb).*sin(2*pi*fc*Tc);
-m=randi([0,1],1,N);
-mod=0;
-message=0;
-for i=1:N
-    if m(i)==1
-        message=[message,ones(1,1000)];
-        mod=[mod,c];
+clc;
+close all;
+clear all;
+
+n=10; % length of bit stream
+b=[1 0 0 1 1 1 0 0 0 1]
+f1=1;f2=2;
+t=0:1/30:1-1/30;
+
+%ASK
+sa1=sin(2*pi*f1*t);
+E1=sum(sa1.^2);
+sa1=sa1/sqrt(E1); %unit energy 
+sa0=0*sin(2*pi*f1*t);
+
+%FSK
+sf0=sin(2*pi*f1*t);
+E0=sum(sf0.^2);
+sf0=sf0/sqrt(E0);
+sf1=sin(2*pi*f2*t);
+E1=sum(sf1.^2);
+sf1=sf1/sqrt(E1);
+
+%PSK
+sp=sin(2*pi*f1*t);
+E1=sum(sp.^2);
+sp0=-sin(2*pi*f1*t)/sqrt(E1);
+sp1=sin(2*pi*f1*t)/sqrt(E1);
+
+%MODULATION
+ask=[];psk=[];fsk=[];
+for i=1:n
+    if b(i)==1
+        ask=[ask sa1];
+        psk=[psk sp1];
+        fsk=[fsk sf1];
     else
-        message=[message,zeros(1,1000)];
-        mod=[mod,c];
+        ask=[ask sa0];
+        psk=[psk sp0];
+        fsk=[fsk sf0];
     end
 end
 
-subplot(4,1,1);
-plot(time,message);
-title("Message signal");
+figure(1)
+subplot(411)
+stairs(0:10,[b(1:10) b(10)],'linewidth',1.5)
+axis([0 10 -0.5 1.5])
+title('Message Bits');grid on
+xlabel('Time');ylabel('Amplitude')
 
-subplot(4,1,2);
-plot(Tc,c);
-title("Carrier signal");
+subplot(412)
+tb=0:1/30:10-1/30;
+plot(tb, ask(1:10*30),'b','linewidth',1.5)
+title('ASK Modulation');grid on
+xlabel('Time');ylabel('Amplitude')
 
-subplot(4,1,3);
-plot(mod);
-title("PSK modulated signal");
+subplot(413)
+plot(tb, fsk(1:10*30),'r','linewidth',1.5)
+title('FSK Modulation');grid on
+xlabel('Time');ylabel('Amplitude');
 
-subplot(4,1,4);
-plot(abs(fft(mod)));
-title("Freq domain");
+subplot(414)
+plot(tb, psk(1:10*30),'k','linewidth',1.5)
+title('PSK Modulation');grid on
+xlabel('Time');
+ylabel('Amplitude');
